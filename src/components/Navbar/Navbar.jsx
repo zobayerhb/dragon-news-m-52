9740c9b731c6f2f8eb78f52d2d894c8739030479
zobayerhb <hosenzobayer55@gmail.com>
2024-11-22 11:05:19 +0600

    project setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import userIcon from "../../assets/user.png";
 const Navbar = () => {
   return (
@@ -13,9 +13,12 @@ const Navbar = () => {
         <div>
           <img src={userIcon} alt="" />
         </div>
-        <button className="btn btn-neutral rounded-none font-semibold text-white">
+        <Link
+          to="/auth/login"
+          className="btn btn-neutral rounded-none font-semibold text-white"
+        >
           Login
-        </button>
+        </Link>
       </div>
     </div>
   );
--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
 import CategoryNews from "../../pages/CategoryNews/CategoryNews";
+import AuthLayout from "../MainLayout/AuthLayout";
+import Login from "../../pages/Login/Login";
+import Register from "../../pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -8,8 +11,8 @@ const router = createBrowserRouter([
     element: <MainLayout></MainLayout>,
     children: [
       {
-        path: '/',
-        element: <Navigate to="/category/01"></Navigate>
+        path: "/",
+        element: <Navigate to="/category/01"></Navigate>,
       },
       {
         path: "/category/:id",
@@ -18,6 +21,20 @@ const router = createBrowserRouter([
           fetch(
             `https://openapi.programming-hero.com/api/news/category/${params.id}`
           ),
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>,
       },
     ],
   },
